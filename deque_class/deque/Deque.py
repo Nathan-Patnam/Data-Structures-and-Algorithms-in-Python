8412a473671f6a490a35e438e070c89fdd8ed2c4
Nathan Patnam <patnamnv@email.wofford.edu>
2018-02-12 22:33:16 -0500

    Fixed

--- deque_class/deque/Deque.py
+++ deque_class/deque/Deque.py
@@ -27,29 +27,32 @@ class Deque:
 
 
     def remove_front(self):
-        temp = self.head
         if self.size ==1:
             set_head_tail(None)
         else:
+            temp = self.head
             self.head = self.head.get_next_node()
         self.size-=1
         return temp.get_data()
     
 
     def remove_rear(self):
-        temp = self.tail
         if self.size == 1:
-            set_head_tail(None)
+            self.__set_head_tail_same_value(None)
         else:
+            temp_data = self.tail.get_data()
+            #update tail
             node_before_tail_reached = False
             temp = self.head
             while node_before_tail_reached != True:
                 if temp.get_next_node() is self.tail:
                     node_before_tail_reached = True
+                else:
+                    temp = temp.get_next_node()
             self.tail = temp
             self.tail.set_next_node(None)
         self.size -=1
-        return temp.get_data()
+        return temp_data
         
 
 
